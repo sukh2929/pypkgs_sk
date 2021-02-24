dff42afb1df3d0aa8f74d029d7aacf60dde7ed18
Sukhdeep kaur <sukhdeep_2929@yahoo.com>
2021-02-24 08:24:23 -0800

    Initail package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs-sk 0.1.0 in a project::
+
+    import pypkgs_sk_0.1.0
---
+++ tests/test_pypkgs_sk_0.1.0.py
@@ -0,0 +1,5 @@
+from pypkgs_sk_0.1.0 import __version__
+from pypkgs_sk_0.1.0 import pypkgs_sk_0.1.0
+
+def test_version():
+    assert __version__ == '0.1.0'
